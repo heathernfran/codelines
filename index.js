2f80775542e56e919a6d5106a4f2c8006d4d42e2
Heather Frantisak <heathernfran@mgail.com>
2016-06-11 06:48:26 -1000

    remove extra q code

--- index.js
+++ index.js
@@ -5,22 +5,3 @@ const Q = require('q'),
 Q.fcall(codelines.getFilePath('Enter project file path:', (input) => {
   return codelines.walkRecursively(input);
 }));
-
-// Q.fcall(codelines.getFilePath('Enter project file path:'))
-//   .then(() => {
-//     return codelines.walkRecursively(input);
-//   })
-//   .then(() => {
-//     return process.exit();
-//   })
-//   .catch(error => console.error());
-
-// codelines.getFilePath('Enter project file path:', (input) => {
-//   Q.fcall(codelines.walkRecursively(input))
-//     .then(() => {
-//       return process.exit();
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// });
