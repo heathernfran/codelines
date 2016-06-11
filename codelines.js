0eef48fcb44e827fb4e7a4d51923479e93ebabb0
Heather Frantisak <heathernfran@mgail.com>
2016-06-10 15:49:04 -1000

    Remove walkThroughProject() and path module, from original read directory function

--- codelines.js
+++ codelines.js
@@ -1,6 +1,5 @@
 const _ = require('lodash'),
       fs = require('fs'),
-      path = require('path'),
       fspromise = require('fs-promise'),
       shelljs = require('shelljs');
 
@@ -36,35 +35,4 @@ module.exports = {
       })
       .catch( err => console.error());
   },
-  walkThroughProject: function(projectDir) {
-    fs.readdir(projectDir, function(error, files) {
-      if (error) {
-        console.log(error);
-        return;
-      } else if (files == "node_modules") {
-        console.log('ignored directory');
-      } else {
-        console.log(files);
-        files.forEach(function(filename) {
-          let filePath = path.join(projectDir, filename),
-              stat = fs.statSync(filePath);
-          if (stat.isFile()) {
-            // callback(filePath, stat);
-            fs.readFile(filePath, function(error, fileContents) {
-              if (error) {
-                console.log(error);
-                return;
-              } else {
-                console.log(fileContents);
-              }
-            });
-            console.log('file: ' + stat.isFile());
-          } else if (stat.isDirectory()) {
-            console.log('directory: ' + stat.isDirectory());
-            // walkThroughProject(filePath);
-          }
-        });
-      }
-    });
-  }
 }
